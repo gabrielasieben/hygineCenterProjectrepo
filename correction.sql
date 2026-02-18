99647f79d1c61184e57bc91d7ef1afe902320a04
Gabriela Sieben <gabriela.sieben@outlook.com>
2026-02-18 21:43:58 +0100

    update correction

--- correction.sql
+++ correction.sql
@@ -1,35 +1,36 @@
--- UPDATE visitor
--- SET first_date =  
--- substr(first_date, 7, 4) || '-' ||
--- substr(first_date, 4, 2) || '-' ||
--- substr(first_date, 1, 2) 
--- WHERE first_date Like '__.__.____';
+-- changing date into another form for better analysing 
+UPDATE visitor
+SET first_date =  
+substr(first_date, 7, 4) || '-' ||
+substr(first_date, 4, 2) || '-' ||
+substr(first_date, 1, 2) 
+WHERE first_date Like '__.__.____';
 
--- SELECT * FROM visitor;
+SELECT * FROM visitor;
 
--- UPDATE article_giveaway
--- SET date =  
--- substr(date, 7, 4) || '-' ||
--- substr(date, 4, 2) || '-' ||
--- substr(date, 1, 2) 
--- WHERE date Like '__.__.____';
+UPDATE article_giveaway
+SET date =  
+substr(date, 7, 4) || '-' ||
+substr(date, 4, 2) || '-' ||
+substr(date, 1, 2) 
+WHERE date Like '__.__.____';
 
--- SELECT * FROM article_giveaway;
+SELECT * FROM article_giveaway;
 
--- UPDATE service_use
--- SET date =  
--- substr(date, 7, 4) || '-' ||
--- substr(date, 4, 2) || '-' ||
--- substr(date, 1, 2) 
--- WHERE date Like '__.__.____';
+UPDATE service_use
+SET date =  
+substr(date, 7, 4) || '-' ||
+substr(date, 4, 2) || '-' ||
+substr(date, 1, 2) 
+WHERE date Like '__.__.____';
 
--- SELECT * FROM service_use;
+SELECT * FROM service_use;
 
--- UPDATE workplan
--- SET date =  
--- substr(date, 7, 4) || '-' ||
--- substr(date, 4, 2) || '-' ||
--- substr(date, 1, 2) 
--- WHERE date Like '__.__.____';
+UPDATE workplan
+SET date =  
+substr(date, 7, 4) || '-' ||
+substr(date, 4, 2) || '-' ||
+substr(date, 1, 2) 
+WHERE date Like '__.__.____';
 
--- SELECT * FROM workplan;
+SELECT * FROM workplan;
